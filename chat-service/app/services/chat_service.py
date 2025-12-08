1dbd63945da7b21c42f6a7990f4311916b435819
Pham An <anpt1@geargames.com>
2025-12-08 21:59:15 +0700

    [FEAT] create query embedding and delete vector db (vector db will use vector db server)

--- chat-service/app/services/chat_service.py
+++ chat-service/app/services/chat_service.py
@@ -3,6 +3,8 @@ from app.utils.open_ai_util import init_open_ai
 client = init_open_ai()
 
 def chat(input: str) -> dict:
+
+
     res = client.responses.create(
         model = "gpt-5-nano",
         input = input
